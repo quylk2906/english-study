26d7f8bde67c0a5ea000a9a28b6d93102613be8e
quylk.uit <quylk2906@gmail.com>
2023-06-26 23:27:28 +0700

    init source code

---
+++ data.ts
@@ -0,0 +1,43 @@
+type Data = {
+  word: string;
+  description: string;
+  sentences: string[];
+};
+
+export const vocabularies: Data[] = [
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+  {
+    word: '__',
+    description: '__',
+    sentences: ['__', '__', '__', '__', '__', '__', '__', '__', '__', '__'],
+  },
+];
